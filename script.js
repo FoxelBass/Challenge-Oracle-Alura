4af936b8a000d0ac0fff59c9086e4038cb959547
FoxelBass <duartezdavid@gmail.com>
2022-01-20 18:28:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var textarea = document.querySelector("#area1");
+
+var button = document.querySelector("button");
+button.onclick = encriptar;
+
+function encriptar() {
+    let texto = textarea.value;
+    console.log(texto);
+}
+
+
+
+//console.log(textarea);
