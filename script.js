84be2a1907cfc91b7e347e3cc749893f7d0fb7ec
FoxelBass <duartezdavid@gmail.com>
2022-01-27 20:34:29 -0300

    update

--- script.js
+++ script.js
@@ -1,44 +1,56 @@
-let entradaUsuario = document.querySelector("#areaUno");
+let entradaUsuario = document.querySelector("#textarea-text");
 let botonEncriptar = document.querySelector("#encriptar");
 let botonDesencriptar = document.querySelector("#desencriptar");
+var botonCopiar = document.querySelector("#copiar");
 
-/* var textarea = document.querySelector("#area1");
-
-var button = document.querySelector("button");
-button.onclick = encriptar;
-
-function encriptar() {
-  let texto = textarea.value;
-  console.log(texto);
-}
-
-//console.log(textarea); */
-
-function validarTexto(texto) {
-  let caracteres = /[~!@#$%^&*()_+|}{[\]\\\/?><:"`;.,áéíóúàèìòù']/g;
+function validarTexto() {
   let mayusculas = /[A-Z]/g;
-  let vacio = "";
+  let caracteresEspeciales = /[áéíóú]/g;
 
-  if (texto.match(mayusculas) || texto.match(caracteres)) {
-    alert("No se permiten caracteres especiales ni mayusculas");
+  if (texto.match(mayusculas)) {
     return true;
-  } else if (texto == vacio) {
-    alert("Ingrese un mensaje para encriptar");
+  } else if (texto.match(caracteresEspeciales)) {
     return true;
   } else {
     return false;
   }
 }
 
-botonEncriptar.addEventListener("click", function () {
-  let textInput = document.querySelector("#input-texto").value;
-  let textoIngresado = textInput;
+//Funcion encriptar
+botonEncriptar.onclick = encriptar;
 
-  if (validarTexto(textoIngresado) == false) {
-    let Encriptado = encriptar(textoIngresado);
-    let resultado = document.querySelector("#msg");
-    resultado.value = Encriptado;
-  } else {
-    textInput = "";
-  }
+function encriptar() {
+  let expSearch = new RegExp("[A-Z]", "g");
+  var text = document.querySelector("#textarea-text").value;
+  var textoEncriptado = text
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+  document.querySelector("#msg").value = textoEncriptado;
+  document.querySelector("#textarea-text").value;
+}
+
+//Funcion desencriptar
+botonDesencriptar.onclick = desencriptar;
+function desencriptar() {
+  let expBusqueda = new RegExp("[A-Z]", "g");
+  var texto = document.querySelector("#textarea-text").value;
+  var textoCifrado = texto
+    .replace(/ai/gi, "a")
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  document.querySelector("#msg").value = textoCifrado;
+  document.querySelector("#textarea-text").value;
+}
+
+//Funcion copiar
+botonCopiar.addEventListener("click", function (boton) {
+  var copiarTexto = document.querySelector("#msg");
+  copiarTexto.select();
+  document.execCommand("copy");
+  document.querySelector("#msg").value = " ";
 });
